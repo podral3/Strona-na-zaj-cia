aa1979bd40bd38aca007605bd8c06681e1380e79
podral3 <podral3@gmail.com>
2024-01-22 17:46:54 +0100

    geojson

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import VectorSource from 'ol/source/Vector.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import {bbox as bboxStrategy} from 'ol/loadingstrategy.js';
+import MousePosition from 'ol/control/MousePosition.js';
+import {ScaleLine, defaults as defaultControls} from 'ol/control.js';
+import {createStringXY} from 'ol/coordinate.js';
+
 
 const vectorSource = new VectorSource({
   format: new GeoJSON(),
@@ -22,6 +26,23 @@ const vectorSource = new VectorSource({
   strategy: bboxStrategy,
 });
 
+const bubdaSource = new VectorSource({
+  features: new GeoJSON().readFeatures(geoJsonBUBDA),
+  style: {
+    'stroke-width': 0.75,
+    'stroke-color': 'black',
+    'fill-color': 'rgba(100,100,100,0.25)',
+  },
+})
+console.log(geoJsonBUBDA)
+const bubdaLayer = new VectorLayer({
+  source: bubdaSource,
+  style: {
+    'stroke-width': 0.75,
+    'stroke-color': 'white',
+    'fill-color': 'rgba(100,100,100,0.25)',
+  },
+})
 const vector = new VectorLayer({
   source: vectorSource,
   style: {
@@ -44,32 +65,32 @@ function createLayer(layerID) {
   return myLayer;
 }
 
+const mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: 'EPSG:4326',
+  className: 'custom-mouse-position',
+  target: document.getElementById('mouse-position'),
+});
+
+const scaleControl = new ScaleLine({
+  units: 'metric',
+  bar: true,
+  steps: 4,
+  text: true,
+  minWidth: 140,
+});
+
 const map = new Map({
+  controls: defaultControls().extend([mousePositionControl, scaleControl]),
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM(),
     }),
-    vector
+    bubdaLayer
   ],
   view: new View({
-    center: [0, 0],
+    center: [14200000, 4130000],
     zoom: 2,
   }),
 });
-
-let ptlz = createLayer("BDOT:PL.PZGiK.283.1218__OT_PTLZ_A — OT_PTLZ_A")
-//map.addLayer(ptlz);
-
-let ptzb = createLayer("BDOT:PL.PZGiK.283.1218__OT_PTZB_A — OT_PTZB_A")
-//map.addLayer(ptzb);
-
-let oior = createLayer("BDOT:PL.PZGiK.283.1218__OT_OIOR_A — OT_OIOR_A")
-//map.addLayer(oior);
-
-let swkn = createLayer("BDOT:PL.PZGiK.283.1218__OT_SWKN_L — OT_SWKN_L")
-//map.addLayer(swkn);
-
-let bubda = createLayer("BDOT:PL.PZGiK.283.1218__OT_BUBD_A inf dod")
-//map.addLayer(bubda);
-  
